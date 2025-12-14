1046ab5e7db4c5ea5c7053cfb37df7860f7f5f90
gowtham-rajolu <gowthamrajolu@gmail.com>
2025-12-15 00:37:57 +0530

    modified:   test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("Test started");
-setTimeout(console.log("Running Test"),1000)
-setTimeout(console.log("Running Test"),1000)
-setTimeout(console.log("Running Test"),1000)
+setTimeout(()=>console.log("Running Test"),1000)
+setTimeout(()=>console.log("Running Test"),1000)
+setTimeout(()=>console.log("Running Test"),1000)
 console.log("Test Done")
